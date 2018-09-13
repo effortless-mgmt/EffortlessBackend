575a760c4bc4559fd89e9516569c45f68057b380
algorythm <anders@wiberg.tech>
2018-09-13 17:09:30 +0200

    User almost done

--- EffortlessApi/Controllers/UserController.cs
+++ EffortlessApi/Controllers/UserController.cs
@@ -51,5 +51,43 @@ namespace EffortlessApi.Controllers
 
             return await _context.Users.FindAsync(user.Id);
         }
+
+        [HttpPut("{username}")]
+        public ActionResult<User> Put(string username, User user)
+        {
+            var oldUser = _context.Users.FirstOrDefault(u => u.Username == username);
+
+            if (oldUser == null) 
+            {
+                return NotFound($"User {user.Username} could not be found.");
+            }
+
+            oldUser.Username = user.Username;
+            oldUser.Firstname = user.Firstname;
+            oldUser.Lastname = user.Lastname;
+            oldUser.Email = user.Email;
+            oldUser.Hours = user.Hours;
+
+            _context.Users.Update(oldUser);
+            _context.SaveChanges();
+
+            return oldUser;
+        }
+
+        [HttpDelete("{username}")]
+        public ActionResult Delete(string username)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == username);
+
+            if (user == null)
+            {
+                return NotFound($"User {username} could not be found.");
+            }
+
+            _context.Remove(user);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
