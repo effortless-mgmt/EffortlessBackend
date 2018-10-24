93ea9bc3ecd99d96c5ac6eb8ce8e587a2a1cbd68
algorythm <anders@wiberg.tech>
2018-10-24 10:55:43 +0200

    Add Identity Server 4

--- EffortlessApi/Models/User.cs
+++ EffortlessApi/Models/User.cs
@@ -1,11 +1,11 @@
+using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace EffortlessApi.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        public long Id { get; set; }
         [Required]
         public string Username { get; set; }
         [Required]
@@ -14,8 +14,6 @@ namespace EffortlessApi.Models
         public string Firstname { get; set; }
         [Required]
         public string Lastname { get; set; }
-        [Required]
-        public string Email { get; set; }
         public List<Hour> Hours { get; set; }
         // public List<Company> Companies { get; set; }
     }
