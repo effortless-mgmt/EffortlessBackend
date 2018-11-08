55133b49853bf195093fcfd220b94bce0fd8e7c6
algorythm <anders@wiberg.tech>
2018-11-08 15:35:16 +0100

    Add all models mirations

--- EffortlessApi/Migrations/20181108143504_add_all_models.Designer.cs
+++ EffortlessApi/Migrations/20181108143504_add_all_models.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using EffortlessApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace EffortlessApi.Migrations
 {
     [DbContext(typeof(EffortlessContext))]
-    partial class EffortlessContextModelSnapshot : ModelSnapshot
+    [Migration("20181108143504_add_all_models")]
+    partial class add_all_models
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
