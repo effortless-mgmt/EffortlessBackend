143b142abd8e939c87f820217ec9ae17be16328c
Thomboline <thomaslienchristensen@gmail.com>
2018-11-18 23:56:14 +0100

    Added required annotation in Agreement model. Closes #21

--- EffortlessApi/Core/Models/Agreement.cs
+++ EffortlessApi/Core/Models/Agreement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EffortlessApi.Core.Models
 {
@@ -6,9 +7,12 @@ namespace EffortlessApi.Core.Models
     {
 
         public long Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Version { get; set; }
+        [Required]
         public decimal UnitPrice { get; set; }
+        [Required]
         public decimal Salary { get; set; }
         public decimal NightSubsidy { get; set; }
         public decimal WeekendSubsidy { get; set; }
