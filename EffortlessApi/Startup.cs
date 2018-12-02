3fe559147155f9574d732c7d49803d698aa97c59
algorythm <anders@wiberg.tech>
2018-12-02 17:40:40 +0100

    Fix formatting in Startup.cs

--- EffortlessApi/Startup.cs
+++ EffortlessApi/Startup.cs
@@ -15,16 +15,19 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Serialization;
 
-namespace EffortlessApi {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
+namespace EffortlessApi{
+    public class Startup
+    {
+        public Startup (IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
+        public void ConfigureServices (IServiceCollection services)
+        {
             var dbHost = Configuration["DB_HOST"] ?? "localhost";
             var dbPort = Configuration["DB_PORT"] ?? "5432";
             var dbUser = Configuration["DB_USER"] ?? "root";
@@ -34,29 +37,33 @@ namespace EffortlessApi {
 
             var connectionString = $"User ID={dbUser}; Password={dbPass}; Server={dbHost}; port={dbPort}; Database=EffortlessApi;Integrated Security=true; Pooling=true;";
 
-            services.AddEntityFrameworkNpgsql().AddDbContext<EffortlessContext>(opt =>
-                opt.UseNpgsql(connectionString));
+            services.AddEntityFrameworkNpgsql().AddDbContext<EffortlessContext>(opt => opt.UseNpgsql(connectionString));
             services.ConfigureCors();
             services.ConfigureAuthorization(authSigningKey);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(o => {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(o => 
+            {
                 o.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
                 o.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IHostingEnvironment env, EffortlessContext context) {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
-            } else {
-                app.UseHsts ();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env, EffortlessContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
             }
 
-            app.UseHttpsRedirection ();
-            context.Database.Migrate ();
-            app.UseCors ("CorsPolicy");
-            app.UseAuthentication ();
-            app.UseMvc ();
+            app.UseHttpsRedirection();
+            context.Database.Migrate();
+            app.UseCors("CorsPolicy");
+            app.UseAuthentication();
+            app.UseMvc();
         }
     }
 }
