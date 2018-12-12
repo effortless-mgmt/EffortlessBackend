38d9180db5818503b79d83f9170b1f347c6396f9
algorythm <anders@wiberg.tech>
2018-12-12 17:40:03 +0100

    Refactored start-docker script a bit

--- start-docker.sh
+++ start-docker.sh
@@ -5,8 +5,31 @@ if [ ! -x "$(which docker)" ]; then
     exit 1	
 fi	
 
-if [ "$1" == "reset" ]; then	
-    echo "Resetting container 'db'."	
+# Stop and remove the container, don't exit afterwards
+if [ "$1" == "reset" ]; then
+	if [ "$(docker ps -q -f name=db)" ]; then	
+		if [ "$(docker ps -aq -f status=running -f name=db)" ]; then	
+			echo "Stopping container 'db'."	
+			docker stop db > /dev/null	
+		fi	
+		echo "Removing container 'db'."
+		docker rm db > /dev/null
+	fi
+fi
+
+# Stop the container and exit, then exit
+if [ "$1" == "stop" ]; then	
+    if [ "$(docker ps -aq -f name=db)" ]; then	
+        if [ "$(docker ps -aq -f status=running -f name=db)" ]; then	
+            docker stop db	
+        fi	
+    fi	
+
+    exit 0
+fi
+
+# Stop and remove the container, then exit
+if [ "$1" == "remove" ]; then	
     if [ "$(docker ps -q -f name=db)" ]; then	
         if [ "$(docker ps -aq -f status=running -f name=db)" ]; then	
             echo "Stopping container 'db'."	
@@ -15,20 +38,17 @@ if [ "$1" == "reset" ]; then
         echo "Removing container 'db'."
         docker rm db > /dev/null
     fi
+
+    exit 0
 fi
 
-if [ "$1" == "stop" ]; then	
-    if [ "$(docker ps -aq -f name=db)" ]; then	
-        if [ "$(docker ps -aq -f status=running -f name=db)" ]; then	
-            docker stop db	
-        fi	
-    fi	
-fi
-
+# If the container exists
 if [ "$(docker ps -aq -f name=db)" ]; then	
+    # If the container exists, but isn't running
     if [ "$(docker ps -aq -f status=exited -f name=db)" ]; then	
         docker start db > /dev/null	
         echo "Docker container 'db' started."	
+    # If the container exists, and is already running
     elif [ "$(docker ps -aq -f status=running -f name=db)" ]; then	
         echo "Docker container 'db' already running."	
     fi	
@@ -36,3 +56,4 @@ else
     docker run --name db -e POSTGRES_USER=root -e POSTGRES_PASSWORD=root -p "5432:5432" -d postgres > /dev/null	
     echo "Created new container named 'db'."	
 fi
+
